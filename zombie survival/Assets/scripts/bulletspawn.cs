03ead68d3a87b3d0f7b36983f29ec5b73a92b35f
YoussifDarwish <youssifmhmoud7@gmail.com>
2023-09-05 16:40:20 +0200

    Update bulletspawn.cs

--- zombie survival/Assets/scripts/bulletspawn.cs
+++ zombie survival/Assets/scripts/bulletspawn.cs
@@ -8,7 +8,7 @@ public class bulletspawn : MonoBehaviour
     [SerializeField] GameObject player;
     void Start()
     {
-        transform.position = new Vector2(player.transform.position.x + 0.68f, player.transform.position.y+ 0.1613339f); 
+        ; 
     }
 
     
